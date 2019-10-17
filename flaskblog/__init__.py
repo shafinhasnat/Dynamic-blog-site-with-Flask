e7297609d087c16b0c996c5a951d7518563981f3
USER <shafinhasnat97@gmail.com>
2019-10-17 19:04:02 +0600

    update

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask, escape, request, render_template,flash,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'shafinsblog'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # app.config['DATABASE_URL'] = 'sqlite:///site.db'
 db=SQLAlchemy(app)
